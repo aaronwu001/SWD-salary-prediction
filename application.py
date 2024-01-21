ee7f07a9ed16f06f34768fb0349696ee67a97be8
aaronwu001 <aaronwu000@gmail.com>
2024-01-20 22:36:45 -0500

    copy the imports from the notebook to application.py

--- application.py
+++ application.py
@@ -3,6 +3,18 @@ from predict_page import show_predict_page
 from explore_page import show_explore_page
 import sklearn
 
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.ticker import AutoMinorLocator, MultipleLocator
+from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
+
+
 page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
 
 if page == "Predict":
